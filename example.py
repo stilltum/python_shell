74a275e93228b5a87970c3bbb2e7e06383eb9338
stilltum <35137975+stilltum@users.noreply.github.com>
2021-06-15 14:19:05 +0800

    Update example.py

--- example.py
+++ example.py
@@ -2,7 +2,9 @@
 # e.g.
 #   for drive in $(ls /dir | grep "sd"):
 
-attached_drives = $(ls /dir | grep "sd")
+# The idea is that all commands within $() are commandline calls, unless it is prepended by another $, in which case it's a script variable
+
+attached_drives = $(ls /dir | grep "sd").splitlines()
 
 mounted_drives = ()
 unmounted_drives = ()
@@ -10,7 +12,12 @@ for drive in attached_drives:
   if $(findmnt $drive):
     mounted_drives += drive
     
-    # run some function or program
+    # some administative output
+    percent = $(df --output=pcent $drive).splitlines()[1][:-1]
+    if percent > 80:
+      print("your drive is getting full!")
   else:
     unmounted_drives += drive
-    # 
+    
+    # some administrative output
+    print("did you mean to mount " + drive + "?")
