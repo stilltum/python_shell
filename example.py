c56e4ad9f63b3d16b09e5ddc218e12a0d29edc00
stilltum <35137975+stilltum@users.noreply.github.com>
2021-06-15 14:22:45 +0800

    Update example.py

--- example.py
+++ example.py
@@ -16,8 +16,8 @@ for drive in attached_drives:
     percent = $(df --output=pcent $drive).splitlines()[1][:-1]
     if percent > 80:
       print("your drive is getting full!")
-  else:
-    unmounted_drives += drive
+    else:
+      unmounted_drives += drive
     
     # some administrative output
     print("did you mean to mount " + drive + "?")
